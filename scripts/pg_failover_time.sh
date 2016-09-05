db1e9adae92c9cf0cba1d630e1cb37e94a229000
panwenhang <panwenhang92@gmail.com>
2016-09-05 17:48:26 +0800

    delete extra space

--- scripts/pg_failover_time.sh
+++ scripts/pg_failover_time.sh
@@ -5,7 +5,7 @@
 declare -x fail_time=0
 export PGPASSWORD=''
 
-while true; do 
+while true; do
     if psql -h server_ip -U postgres -Atqc 'select 1;' &>/dev/null; then
         if (( $fail_time > 0  )); then
             echo 故障恢复时间:$(date +'%F %T')
@@ -16,5 +16,5 @@ while true; do
             echo 故障开始时间:$(date +'%F %T')
         fi
             fail_time+=1
-    fi 
+    fi
 done
